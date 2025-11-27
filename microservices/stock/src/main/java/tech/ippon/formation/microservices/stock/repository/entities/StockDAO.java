8573819812983dcf5e74c58e0f3afe84c22fdb33
Alexandre Bruyant <alexandre@bruyant.xyz>
2025-11-27 10:57:57 +0100

    DO IT

--- microservices/stock/src/main/java/tech/ippon/formation/microservices/stock/repository/entities/StockDAO.java
+++ microservices/stock/src/main/java/tech/ippon/formation/microservices/stock/repository/entities/StockDAO.java
@@ -1,7 +1,7 @@
 package tech.ippon.formation.microservices.stock.repository.entities;
 
-
 import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -10,23 +10,23 @@ import tech.ippon.formation.microservices.stock.domain.Stock;
 @Entity(name = "stocks")
 public class StockDAO {
 
-  @Id
-  @Column(name = "product_id", updatable = false, nullable = false)
-  private UUID productId;
+    @Id
+    @Column(name = "product_id", updatable = false, nullable = false)
+    private UUID productId;
 
-  @Column(name = "stock")
-  private long stock;
+    @Column(name = "stock")
+    private long stock;
 
-  public Stock toDTO(){
-    return new Stock(productId.toString(), stock);
-  }
+    public Stock toDTO() {
+        return new Stock(productId.toString(), stock);
+    }
 
-  public long getStock() {
-    return stock;
-  }
+    public long getStock() {
+        return stock;
+    }
 
-  public StockDAO setStock(long stock) {
-    this.stock = stock;
-    return this;
-  }
+    public StockDAO setStock(long stock) {
+        this.stock = stock;
+        return this;
+    }
 }
