8d1fdcb8bcad1f42314fface25885a5ccee54678
Alexandre Bruyant <alexandre@bruyant.xyz>
2025-11-27 10:56:27 +0100

    bug fix

--- microservices/stock/src/main/java/tech/ippon/formation/microservices/stock/services/StockService.java
+++ microservices/stock/src/main/java/tech/ippon/formation/microservices/stock/services/StockService.java
@@ -1,10 +1,12 @@
 package tech.ippon.formation.microservices.stock.services;
 
 import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
 import tech.ippon.formation.microservices.stock.domain.Order;
 import tech.ippon.formation.microservices.stock.domain.Product;
 import tech.ippon.formation.microservices.stock.domain.Stock;
@@ -15,54 +17,49 @@ import tech.ippon.formation.microservices.stock.repository.StockRepository;
 @Service
 public class StockService {
 
-  private final Logger logger = LoggerFactory.getLogger(StockService.class);
+    private final Logger logger = LoggerFactory.getLogger(StockService.class);
 
-  private final StockRepository stockRepository;
+    private final StockRepository stockRepository;
 
-  public StockService(StockRepository stockRepository) {
-    this.stockRepository = stockRepository;
-  }
-
-  public Stock getByProductId(String id) {
-    try {
-      if (Math.random() >= 0.75) {
-        Thread.sleep(1100);
-      }
-      return stockRepository.findById(UUID.fromString(id))
-          .orElseThrow(ProductNotFoundException::new).toDTO();
-    } catch (IllegalArgumentException e) {
-      throw new IncorrectIdFormatException();
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
     }
-  }
 
-  @KafkaListener(topics = "orders")
-  public void onOrder(Order order) {
-    logger.info("Received: {}", order);
-
-    if (order != null && order.getCart() != null && order.getCart().getProducts() != null) {
-      order.getCart().getProducts().forEach(this::decreaseStock);
+    public Stock getByProductId(String id) {
+        try {
+            return stockRepository.findById(UUID.fromString(id))
+                    .orElseThrow(ProductNotFoundException::new).toDTO();
+        } catch (IllegalArgumentException e) {
+            throw new IncorrectIdFormatException();
+        }
     }
-  }
 
-  private void decreaseStock(Product product) {
-    final var optionalStock = stockRepository.findById(UUID.fromString(product.getId()));
-    if (optionalStock.isPresent()) {
-      final var stock = optionalStock.get();
-      var newStock = stock.getStock() - product.getQuantity();
-      stock.setStock(newStock);
-      stockRepository.save(stock);
-    }
-  }
+    @KafkaListener(topics = "orders")
+    public void onOrder(Order order) {
+        logger.info("Received: {}", order);
 
-  public void addStock(String productId, int stockToAdd) {
-    final var optionalStock = stockRepository.findById(UUID.fromString(productId));
-    if (optionalStock.isPresent()) {
-      final var stock = optionalStock.get();
-      var newStock = stock.getStock() + stockToAdd;
-      stock.setStock(newStock);
-      stockRepository.save(stock);
+        if (order != null && order.getCart() != null && order.getCart().getProducts() != null) {
+            order.getCart().getProducts().forEach(this::decreaseStock);
+        }
     }
-  }
+
+    private void decreaseStock(Product product) {
+        final var optionalStock = stockRepository.findById(UUID.fromString(product.getId()));
+        if (optionalStock.isPresent()) {
+            final var stock = optionalStock.get();
+            var newStock = stock.getStock() - product.getQuantity();
+            stock.setStock(newStock);
+            stockRepository.save(stock);
+        }
+    }
+
+    public void addStock(String productId, int stockToAdd) {
+        final var optionalStock = stockRepository.findById(UUID.fromString(productId));
+        if (optionalStock.isPresent()) {
+            final var stock = optionalStock.get();
+            var newStock = stock.getStock() + stockToAdd;
+            stock.setStock(newStock);
+            stockRepository.save(stock);
+        }
+    }
 }
